c4e98118b7edfca84aca8f30516d3c70d518eb58
MukeshSelvan M <mk.mukeshselvan@gmail.com>
2024-11-25 21:58:45 +0530

    late submission

--- dash.js
+++ dash.js
@@ -2,10 +2,10 @@ var ctx1 = document.getElementById('lineChart1').getContext('2d');
         var lineChart1 = new Chart(ctx1, {
             type: 'line',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: ['week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7'],
                 datasets: [{
                     label: 'NITROGEN',
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    data: [26, 35, 39, 38, 41.2, 40, 29],
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 3, // Increased line width
                     fill: false
@@ -25,10 +25,10 @@ var ctx1 = document.getElementById('lineChart1').getContext('2d');
         var lineChart1 = new Chart(ctx1, {
             type: 'line',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: ['week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7'],
                 datasets: [{
                     label: 'POTASSIUM',
-                    data: [23, 45, 42, 78, 67, 56, 56],
+                    data: [230, 255, 287, 296, 289, 301, 322.75],
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 3, // Increased line width
                     fill: false
@@ -46,10 +46,10 @@ var ctx1 = document.getElementById('lineChart1').getContext('2d');
         var lineChart1 = new Chart(ctx1, {
             type: 'line',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: ['week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7'],
                 datasets: [{
                     label: 'PHOSPHORUS',
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    data: [650, 590, 680, 671, 660, 720, 750],
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 3, // Increased line width
                     fill: false
@@ -67,10 +67,10 @@ var ctx1 = document.getElementById('lineChart1').getContext('2d');
         var lineChart1 = new Chart(ctx1, {
             type: 'line',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: ['week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7'],
                 datasets: [{
                     label: 'SOIL MOISTURE',
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    data: [55, 54, 49, 58, 48.5,42.5,57.5],
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 3, // Increased line width
                     fill: false
@@ -88,10 +88,10 @@ var ctx1 = document.getElementById('lineChart1').getContext('2d');
         var lineChart1 = new Chart(ctx1, {
             type: 'line',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: ['week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7'],
                 datasets: [{
                     label: 'SOIL TEMPERATURE',
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    data: [22, 27, 30.84, 24.55, 20.5, 28.5, 31.55],
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 3, // Increased line width
                     fill: false
@@ -109,10 +109,10 @@ var ctx1 = document.getElementById('lineChart1').getContext('2d');
         var lineChart1 = new Chart(ctx1, {
             type: 'line',
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: ['week1', 'week2', 'week3', 'week4', 'week5', 'week6', 'week7'],
                 datasets: [{
                     label: 'WATER PH',
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    data: [7.1, 6.8, 8.2, 7.5, 6.9, 8.0, 7.3],
                     borderColor: 'rgba(75, 192, 192, 1)',
                     borderWidth: 3, // Increased line width
                     fill: false
